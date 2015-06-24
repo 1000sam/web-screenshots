51d50e107961c847eb38a17f019d55d49604d7bb
1000sam <samuel.vatier@gmail.com>
2015-06-24 15:09:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ def index():
     wait_time = float(request.args.get("t", 20)) / 1000  # ms
 
     if not url:
-        return "Example: <a href='http://scrn.herokuapp.com/?url=http://e.cdn.1000mercis.com.s3.amazonaws.com/0_service_qualite/livetweets/&w=1200'>" \
-               "http://scrn.herokuapp.com/?url=http://e.cdn.1000mercis.com.s3.amazonaws.com/0_service_qualite/livetweets/</a>"
+        return "http://scrn.herokuapp.com/?url=http://e.cdn.1000mercis.com.s3.amazonaws.com/0_service_qualite/livetweets/index.html&w=1200"
 
     driver = Driver()
     driver.set_window_position(0, 0)
